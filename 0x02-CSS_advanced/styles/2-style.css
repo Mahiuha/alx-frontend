601affc60d8cc4a32a6ef4f9745528002d487c8d
Mahiuha <tiasho.mahiuha@gmail.com>
2023-02-15 00:20:01 +0300

    color values

---
+++ 0x02-CSS_advanced/styles/2-style.css
@@ -0,0 +1,25 @@
+/* add colors */
+
+html {
+    scroll-behavior: smooth;
+}
+
+body {
+    color: #161616;
+}
+
+a {
+    color: #161616;
+}
+
+.visually-hidden {
+    display: none;
+}
+
+.card-category {
+    color: #D73953;
+}
+
+.section-tagline {
+    color: #D73953;
+}
